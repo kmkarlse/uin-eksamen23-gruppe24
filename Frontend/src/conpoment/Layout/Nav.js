c8884c600de784fac00e34016a07c1e2182a0cc9
kmkarlse <77330597+kmkarlse@users.noreply.github.com>
2023-05-14 22:15:43 +0200

    more

--- Frontend/src/conpoment/Layout/Nav.js
+++ Frontend/src/conpoment/Layout/Nav.js
@@ -1,24 +1,19 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 //import { ReactComponent as Logo } from "../../Logo.svg";
 
 export default function Nav() {
-  const [drop, setDrop] = useState(false);
-
   return (
     <>
       <Link className="headerfont" to="/dash">
         GameShop
       </Link>
-      <nav>
-        <button onClick={() => setDrop(!drop)}>menu</button>
-        {drop && (
-          <div className="menu">
-            <Link to="MyGames">MyGames</Link>
-            <Link to="MyFavorites">MyFavorites</Link>
-            <Link to="GameShop">GameShop</Link>
-          </div>
-        )}
+      <nav className="dropdown">
+        <button className="dropbtn">Menu</button>
+        <div className="dropdown-content">
+          <Link to="MyGames">MyGames</Link>
+          <Link to="MyFavorites">MyFavorites</Link>
+          <Link to="GameShop">GameShop</Link>
+        </div>
       </nav>
     </>
   );
